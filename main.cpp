d065cdeb9bebda787ad7b3dab190478dc6196fc4
Julien <Julien>
2017-06-24 20:41:58 +0200

    add Rules

--- main.cpp
+++ main.cpp
@@ -6,8 +6,52 @@
 #include <vector>
 #include <algorithm>
 #include <assert.h>
+#include "gtest/gtest.h"
 
 using namespace std;
+
+enum ACTION_TYPE
+{
+	MOVEBUILD, MOVE, BUILD
+};
+
+enum DIRECTION
+{
+	N, NE, E, SE, S, SW, W, NW
+};
+
+
+static string to_string(ACTION_TYPE actionType) {
+	switch (actionType) {
+		case MOVEBUILD:
+			return "MOVE&BUILD";
+		case MOVE:
+			return "MOVE";
+		case BUILD:
+			return "BUILD";
+	}
+}
+
+static string to_string(DIRECTION direction) {
+	switch (direction) {
+		case N:
+			return "N";
+		case NE:
+			return "NE";
+		case E:
+			return "E";
+		case SE:
+			return "SE";
+		case S:
+			return "S";
+		case SW:
+			return "SW";
+		case W:
+			return "W";
+		case NW:
+			return "NW";
+	}
+}
 
 struct Point
 {
@@ -54,6 +98,28 @@ private:
 	vector<Unit> otherUnits;
 };
 
+class Action {
+public:
+	Action() {}
+	string getAsString() const {
+		return to_string(actionType) + " " + to_string(index) + " " + to_string(dirMove) + " " + to_string(dirBuild);
+	}
+
+private:
+	ACTION_TYPE actionType;
+	int index;
+	DIRECTION dirMove;
+	DIRECTION dirBuild;
+};
+
+class Rules {
+public:
+	Rules(vector<Action> a) : actions(a) {};
+	vector<Action> getActions() const { return actions; }
+
+private:
+	vector<Action> actions;
+};
 
 
 string generateAction(const int& counter, const vector<string> &legalActions) {
@@ -161,7 +227,7 @@ int main()
 /*
 	main for local test
 */
-int main() {
+int main(int argc, char** argv) {
 	int counter = 1;
 
 	int size = 5;
@@ -227,4 +293,8 @@ int main() {
 	assert(generateAction(counter, legalActions) == "MOVE&BUILD 0 S N");
 
 	cout << generateAction(counter, legalActions) << endl;;
+
+	testing::InitGoogleTest(&argc, argv);
+	RUN_ALL_TESTS(); 
+
 }
