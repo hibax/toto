fd92f761a5dad4f0ec7ba83734a2de8c7ddcea3f
Julien <Julien>
2017-06-24 16:58:37 +0200

    add local test case

--- main.cpp
+++ main.cpp
@@ -8,15 +8,30 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <assert.h>
 
 using namespace std;
-
 
 struct Point
 {
 	int x;
 	int y;
 };
+
+class Map {
+public:
+	Map(const vector<vector<int>> &cells) : cells(cells) {};
+
+	int getCell(const Point &p) const { return cells.at(p.x).at(p.y); }
+	int getCell(const int &x, const int &y) const { return cells.at(x).at(y); }
+	void setCell(const Point &p, const int &i) { cells.at(p.x).at(p.y) = i; }
+	void setCell(const int &x, const int &y, const int &i) { cells.at(x).at(y) = i; }
+
+
+private:
+	vector<vector<int>> cells;
+};
+
 
 
 string generateAction(const int& counter, const vector<string> &legalActions) {
@@ -33,9 +48,12 @@ string generateAction(const int& counter, const vector<string> &legalActions) {
 	{
 		action = legalActions.at(0);
 	}
+	return action;
 }
 
 
+/*
+	Uncomment in Coding Game
 
 int main()
 {
@@ -82,4 +100,64 @@ int main()
 		cout << generateAction(counter, legalActions) << endl;
 
 	}
+}
+*/
+
+/*
+	main for local test
+*/
+int main() {
+	int counter = 1;
+
+	int size = 5;
+	int unitsPerPlayer = 1;
+	vector<string> rows;
+	rows.push_back("00000");
+	rows.push_back("00000");
+	rows.push_back("04104");
+	rows.push_back("03234");
+	rows.push_back("00003");
+
+	vector<Point> myUnits;
+	myUnits.push_back({2, 2});
+	vector<Point> otherUnits;
+	myUnits.push_back({3, 3});
+
+	vector<string> legalActions;
+	legalActions.push_back("MOVE&BUILD 0 E N");
+	legalActions.push_back("MOVE&BUILD 0 E NE");
+	legalActions.push_back("MOVE&BUILD 0 E NW");
+	legalActions.push_back("MOVE&BUILD 0 E SW");
+	legalActions.push_back("MOVE&BUILD 0 E W");
+	legalActions.push_back("MOVE&BUILD 0 N E");
+	legalActions.push_back("MOVE&BUILD 0 N N");
+	legalActions.push_back("MOVE&BUILD 0 N NE");
+	legalActions.push_back("MOVE&BUILD 0 N NW");
+	legalActions.push_back("MOVE&BUILD 0 N S");
+	legalActions.push_back("MOVE&BUILD 0 N SE");
+	legalActions.push_back("MOVE&BUILD 0 N W");
+	legalActions.push_back("MOVE&BUILD 0 NE E");
+	legalActions.push_back("MOVE&BUILD 0 NE N");
+	legalActions.push_back("MOVE&BUILD 0 NE NE");
+	legalActions.push_back("MOVE&BUILD 0 NE NW");
+	legalActions.push_back("MOVE&BUILD 0 NE S");
+	legalActions.push_back("MOVE&BUILD 0 NE SW");
+	legalActions.push_back("MOVE&BUILD 0 NE W");
+	legalActions.push_back("MOVE&BUILD 0 NW E");
+	legalActions.push_back("MOVE&BUILD 0 NW N");
+	legalActions.push_back("MOVE&BUILD 0 NW NE");
+	legalActions.push_back("MOVE&BUILD 0 NW NW");
+	legalActions.push_back("MOVE&BUILD 0 NW SE");
+	legalActions.push_back("MOVE&BUILD 0 NW SW");
+	legalActions.push_back("MOVE&BUILD 0 NW W");
+	legalActions.push_back("MOVE&BUILD 0 S N");
+	legalActions.push_back("MOVE&BUILD 0 S NE");
+	legalActions.push_back("MOVE&BUILD 0 S S");
+	legalActions.push_back("MOVE&BUILD 0 S SE");
+	legalActions.push_back("MOVE&BUILD 0 S SW");
+	legalActions.push_back("MOVE&BUILD 0 S W");
+
+	assert(generateAction(counter, legalActions) == "MOVE&BUILD 0 S N");
+
+	cout << generateAction(counter, legalActions) << endl;;
 }
