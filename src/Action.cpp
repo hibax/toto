b87a48c51e50251c5b911efbf6fc113521be2dab
Hibax <julienflb.iie@gmail.com>
2017-06-27 00:52:51 +0200

    fix to_string ambiguous

--- src/Action.cpp
+++ src/Action.cpp
@@ -1,41 +1,39 @@
 #include "Action.h"
 
-namespace Utils {
-	static string to_string(ACTION_TYPE actionType) {
-		switch (actionType) {
-		case MOVEBUILD:
-			return "MOVE&BUILD";
-		case MOVE:
-			return "MOVE";
-		case BUILD:
-			return "BUILD";
-		default:
-			return "";
-		}
+static string to_str(ACTION_TYPE actionType) {
+	switch (actionType) {
+	case MOVEBUILD:
+		return "MOVE&BUILD";
+	case MOVE:
+		return "MOVE";
+	case BUILD:
+		return "BUILD";
+	default:
+		return "";
 	}
+}
 
 
-	static string to_string(DIRECTION direction) {
-		switch (direction) {
-		case N:
-			return "N";
-		case NE:
-			return "NE";
-		case E:
-			return "E";
-		case SE:
-			return "SE";
-		case S:
-			return "S";
-		case SW:
-			return "SW";
-		case W:
-			return "W";
-		case NW:
-			return "NW";
-		default:
-			return "";
-		}
+static string to_str(DIRECTION direction) {
+	switch (direction) {
+	case N:
+		return "N";
+	case NE:
+		return "NE";
+	case E:
+		return "E";
+	case SE:
+		return "SE";
+	case S:
+		return "S";
+	case SW:
+		return "SW";
+	case W:
+		return "W";
+	case NW:
+		return "NW";
+	default:
+		return "";
 	}
 }
 
@@ -44,7 +42,7 @@ Action::Action(ACTION_TYPE at, int i, DIRECTION dm, DIRECTION db):
 	actionType(at), index(i), dirMove(dm), dirBuild(db) {}
 
 string Action::getAsString() const {
-	return Utils::to_string(actionType) + " " + std::to_string(index) + " " + Utils::to_string(dirMove) + " " + Utils::to_string(dirBuild);
+	return to_str(actionType) + " " + to_string(index) + " " + to_str(dirMove) + " " + to_str(dirBuild);
 }
 
 void Action::setActionType(ACTION_TYPE at) { actionType = at; }
