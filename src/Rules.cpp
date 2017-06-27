635759d43054c3cb4786f768bac94cf6ad49b6ab
Hibax <julienflb.iie@gmail.com>
2017-06-27 21:02:51 +0200

    pair <Action, Board>

--- src/Rules.cpp
+++ src/Rules.cpp
@@ -10,14 +10,15 @@ using namespace std;
 namespace GameRules {
 
 	template <>
-	vector<Board> produceNextStates(const Board & state, bool myTurn) {
+	vector<pair<Action, Board> > produceNextStates(const pair<Action, Board> & state, bool myTurn) {
 
-		vector<Board> nextStates;
-		vector<Action> actions = state.produceAllLegalActions(myTurn);
+		vector<pair<Action, Board> > nextStates;
 
+		Board currentBoard = get<1>(state);
+		vector<Action> actions = currentBoard.produceAllLegalActions(myTurn);
 
 		for (Action action : actions) {
-			nextStates.push_back(state.play(action));
+			nextStates.push_back(make_pair(action, currentBoard.play(action)));
 		}
 
 		return nextStates;
